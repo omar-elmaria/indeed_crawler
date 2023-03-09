7e53fad5a3d575d6b57719883fbf427f7dec2d9e
Omar ElMaria <omar.elmaria@deliveryhero.com>
2023-03-09 21:55:37 +0100

    add log file and navigation to correct directory

--- indeed/main_scraper_1.py
+++ indeed/main_scraper_1.py
@@ -1,10 +1,18 @@
 from scrapy.crawler import CrawlerProcess
 from functions import post_crawling_func
+import os
 
 def main_scraper_1():
+    # If you are not in indeed/, navigate to it
+    if "chris_begin_indeed/indeed" not in os.getcwd():
+        os.chdir(os.getcwd() + "/indeed")
+
     # Run the first crawler that crawls this link --> https://ca.indeed.com/jobs?l=Greater+Toronto+Area%2C+ON&sc=0kf%3Aocc%286YCJB%29%3B&radius=35&sort=date&vjk=f55ce01235a88065
     from indeed.spiders.indeed_zyte_api_1 import IndeedZyteAPI1Spider
-    process = CrawlerProcess(settings={"FEEDS": {"output_indeed_zyte_api_1.json":{"format": "json", "overwrite": True, "encoding": "utf-8"}}})
+    process = CrawlerProcess(settings={
+        "FEEDS": {"output_indeed_zyte_api_1.json":{"format": "json", "overwrite": True, "encoding": "utf-8"}},
+        "LOG_FILE": "indeed_zyte_api_1.log"
+    })
     process.crawl(IndeedZyteAPI1Spider)
     process.start()
 
